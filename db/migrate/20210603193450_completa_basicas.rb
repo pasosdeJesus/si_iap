a330bfd8c29dfedc53eee782ed376da9b51f9afc
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-06-04 05:31:17 -0500

    Menu y opciones tablas basicas

--- db/migrate/20210603193450_completa_basicas.rb
+++ db/migrate/20210603193450_completa_basicas.rb
@@ -2,20 +2,20 @@ class CompletaBasicas < ActiveRecord::Migration[6.1]
   def up
     execute <<-SQL
 
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (1, 'SIN INFORMACIÓN', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (2, 'SINDICAL', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (3, 'COOPERATIVA', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (4, 'PRODUCTIVA', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (5, 'CORPORACIÓN', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (6, 'ASOCIACIÓN', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (7, 'FUNDACIÓN', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (1, 'SIN INFORMACIÓN', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (2, 'SINDICAL', NULL, '2021-06-03', '2021-06-03', '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (3, 'COOPERATIVA', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (4, 'PRODUCTIVA', NULL, '2021-06-03', '2021-06-03', '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (5, 'CORPORACIÓN', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (6, 'ASOCIACIÓN', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.tipoorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (7, 'FUNDACIÓN', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
 
       SELECT pg_catalog.setval('public.tipoorganzorc_id_seq', 100, true);
 
 
-      INSERT INTO public.nivelorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (1, 'SIN INFORMACIÓN', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.nivelorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (2, 'BASE', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
-      INSERT INTO public.nivelorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (3, 'ORGANIZACIÓN DE ORGANIZACIONES', NULL, '2020-02-28', NULL, '2020-02-28 00:00:00', '2020-02-28 00:00:00');
+      INSERT INTO public.nivelorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (1, 'SIN INFORMACIÓN', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.nivelorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (2, 'PRIMERO', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
+      INSERT INTO public.nivelorganzorc (id, nombre, observaciones, fechacreacion, fechadeshabilitacion, created_at, updated_at) VALUES (3, 'SEGUNDO', NULL, '2021-06-03', NULL, '2021-06-03 00:00:00', '2021-06-03 00:00:00');
 
       SELECT pg_catalog.setval('public.nivelorganzorc_id_seq', 100, true);
 
