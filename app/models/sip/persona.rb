da45b9e3f370bdb30b38341deedb8bba1d4de584
Alejandro Cruz <alejocruzzz@gmail.com>
2021-06-19 09:32:18 -0500

    cambio innecesario

--- app/models/sip/persona.rb
+++ app/models/sip/persona.rb
@@ -1,13 +1,11 @@
 # encoding: UTF-8
 
-require 'sip/concerns/models/persona'
 require 'sivel2_gen/concerns/models/persona'
 require 'cor1440_gen/concerns/models/persona'
 
 module Sip
   class Persona < ActiveRecord::Base
 
-    include Sip::Concerns::Models::Persona
     include Sivel2Gen::Concerns::Models::Persona
     include Cor1440Gen::Concerns::Models::Persona
 
