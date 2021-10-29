2454c4560b380d360a2c0b2b7002b4aad74958e9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-29 04:26:03 -0500

    Ortografia y orden js

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -23,21 +23,22 @@ Rails.start()
 import Turbolinks from "turbolinks"
 Turbolinks.start()
 
+import $ from "expose-loader?exposes=$,jQuery!jquery"
+import 'jquery-ui'
+import 'jquery-ui/ui/widgets/autocomplete'
+import 'jquery-ui/ui/focusable'
+import 'jquery-ui/ui/data'
+import 'jquery-ui/ui/widgets/tooltip'
+import 'popper.js'              // Dialogos emergentes usados por bootstrap
+import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
+import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
+import 'bootstrap-datepicker'
+import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
+
 import ApexCharts from 'apexcharts'
 window.ApexCharts = ApexCharts
 
 var L = require('leaflet');
 var mc= require('leaflet.markercluster');
 
-import $ from "expose-loader?exposes=$,jQuery!jquery"
-
-import 'popper.js'              // Dialogos emergentes usados por bootstrap
-import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
-import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
-import 'bootstrap-datepicker'
-import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/autocomplete'
-import 'jquery-ui/ui/focusable'
-import 'jquery-ui/ui/data'
 
