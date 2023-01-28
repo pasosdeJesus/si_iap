f27cd0a47fa42105c5adc09dcd227b6df80e2d4a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-27 20:55:44 -0500

    Infraestructura

--- db/migrate/20220525122100_renombra_estadoobs.rb
+++
@@ -1,6 +0,0 @@
-class RenombraEstadoobs < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :estadoobs, :sip_estadosol
-    rename_column :sip_solicitud, :estadoobs_id, :estadosol_id
-  end
-end
